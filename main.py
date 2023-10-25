3501afb40c882e062f87aa523aa93963b168dca2
StoopidBoi-6128 <149012620+StoopidBoi-6128@users.noreply.github.com>
2023-10-25 16:59:20 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
-# Code by ETHAN CALBY
+print("Menu")
+print("-------------")
+print("1. Encode")
+print("2. Decode")
+print("3. Quit")
+
+choice = int(input('Please enter an option:'))
+if choice == 1:
+    encode()
+if choice == 2:
+    decode()
+if choice == 3:
+    break
+
+
 
 def encode():
-    password = [1, 2, 3, 4, 5]
+    password = int(input('Please enter your password to encode:')
     for char in range(len(password)):
         password[char] += 3
-    print(password)
+    print("Your password has been encoded and stored!")
     return password
 
 
@@ -13,8 +27,3 @@ def decode():
     for char in range(len(password)):
         password[char] -= 3
     print(password)
-
-
-if __name__ == '__main__':
-    encode()
-    decode()
