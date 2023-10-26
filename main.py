04f3c831ec70fb513199c216a555df55e0688676
StoopidBoi-6128 <149012620+StoopidBoi-6128@users.noreply.github.com>
2023-10-26 11:07:06 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,38 @@
-#code by Ethan Calby
-
-print("Menu")
-print("-------------")
-print("1. Encode")
-print("2. Decode")
-print("3. Quit")
-
-choice = int(input('Please enter an option:'))
-if choice == 1:
-    encode()
-if choice == 2:
-    decode()
-if choice == 3:
-    break
+# code by Ethan Calby and Karen Tong
 
 
+def encoder():
+    global encoded
+    password = input("Please enter your password to encode: ")
+    print("Your password has been encoded and stored!\n")
+    encoded = ""
+    for char in password:
+        encoded += str(int(char) + 3)
+        print(password)
+        print(encoded)
+    return encoded
 
-def encode():
-    password = int(input('Please enter your password to encode:')
-    for char in range(len(password)):
-        password[char] += 3
-    print("Your password has been encoded and stored!")
-    return password
+
+def decoder(encoded):
+    decoded = ""
+    for char in encoded:
+        decoded += str(int(char) - 3)
+    print("The encoded password is ", {encoded}, ", and the original password is ", {decoded}, ".")
+    return decoded
 
 
-def decode():
-    password = [1, 2, 3, 4, 5]
-    for char in range(len(password)):
-        password[char] -= 3
-    print(password)
+def main():
+    global password
+    while True:
+        print("Menu\n-------------\n1. Encode\n2. Decode\n3. Quit\n")
+        menu = int(input("Please enter an option: "))
+        if menu == 1:
+            password = encoder()
+        elif menu == 2:
+            decoder(encoded)
+        elif menu == 3:
+            break
+
+
+if __name__ == "__main__":
+    main()
